34356cafefbfcb517ec35c6adc5e656731dbcffb
Idan Lapid <idan.lapid@cellebrite.com>
2018-11-20 17:16:27 +0200

    Started: AngularCLI todo list
    Signed-off-by: Idan Lapid <idan.lapid@cellebrite.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TodoListComponent} from './todo-list/todo-list.component';
+import {TodoItemComponent} from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
+    TodoItemComponent,
   ],
   imports: [
     BrowserModule,
